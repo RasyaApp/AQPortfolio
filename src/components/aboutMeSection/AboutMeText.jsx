d5aa1af08c39b1f9719369beb79cc88f72532f02
RasyaApp <hitayo015@gmail.com>
2025-02-12 11:14:16 +0700

    12/02/2025

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -5,13 +5,12 @@ const AboutMeText = () => {
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p>
-        I’m Abdelrahman, a web developer instructor with a passion for teaching
-        and coding. I specialize in React and front-end development, helping
-        students build real-world projects and master modern web technologies. I
-        also run a YouTube channel, CodeNest, where I create tutorials and
-        courses to guide aspiring developers in their journey toward successful
-        careers in tech. Outside of coding, I enjoy continuous learning and
-        sharing knowledge to inspire others to achieve their goals.
+        I am a Designer and Front-End Developer focused on creating engaging and responsive 
+        digital experiences. With expertise in React and Tailwind CSS, I am able to translate 
+        designs into functional and aesthetic interfaces.On the design side, I use Figma to 
+        create modern and intuitive visual concepts, ensuring every detail supports an optimal 
+        user experience. I am always eager to learn new technologies, overcome challenges, and 
+        create impactful solutions.
       </p>
       <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
         <Link
